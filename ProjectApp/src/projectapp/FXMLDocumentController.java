ced38a1e7c8e34e912cf9394194f8a5e89456f3d
Sbangro69 <pancraziocirillo8@gmail.com>
2022-11-23 11:23:39 +0100

    scrollpane

--- ProjectApp/src/projectapp/FXMLDocumentController.java
+++ ProjectApp/src/projectapp/FXMLDocumentController.java
@@ -10,12 +10,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author pasqualecaggiano
  */
 public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private AnchorPane scrollPane;
     
   
     @Override
