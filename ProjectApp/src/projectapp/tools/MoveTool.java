3dbc26dae09b17853dbcfcfddbbabd44eab5daf7
xSaba17 <s.destefano28@studenti.unisa.it>
2022-12-02 10:22:02 +0100

    Update MoveTool and ResizeTool

--- ProjectApp/src/projectapp/tools/MoveTool.java
+++ ProjectApp/src/projectapp/tools/MoveTool.java
@@ -4,19 +4,11 @@
  */
 package projectapp.tools;
 
-
-/**
- *
- * @author pasqualecaggiano
- */
-
-    
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
 import projectapp.command.CommandExecutor;
-import projectapp.command.MoveCommand;
 
 /**
  *
@@ -24,25 +16,57 @@ import projectapp.command.MoveCommand;
  */
 public class MoveTool extends SelectionTool{
     
-       
-    public MoveTool(Pane pane,Shape selectedShape,CommandExecutor executor) {
-        super(pane,selectedShape,executor); 
-    }
+    private double initialPositionX;
+    private double initialPositionY;
+    private boolean flag;
     
+    public MoveTool(Pane pane, Shape selectedShape, CommandExecutor executor) {
+        super(pane, selectedShape, executor);
+    }
+
     @Override
     public void changeBorderColor(Color strokeColor) {}
 
     @Override
     public void changeInteriorColor(Color strokeColor) {}
-
+    
+    @Override
+    public void onMousePressed(MouseEvent event, Color strokeColor, Color fillColor){
+        if (super.getSelectedShape() != null){
+           super.getSelectedShape().setStyle("-fx-stroke-dash-array:none");
+        }
+        
+        if (event.getTarget().getClass()!= getPane().getClass()){
+            super.setSelectedShape((Shape) event.getTarget());
+            super.getSelectedShape().setStyle("-fx-stroke-dash-array:5px");
+            flag = true;
+            this.initialPositionX = event.getX();
+            this.initialPositionY = event.getY();
+        }
+        else{
+            flag = false;
+        }
+    }
+    
+    
     @Override
     public void onMouseDragged(MouseEvent event) {
-        getSelectedShape().relocate(event.getX(),event.getY());
+        if(flag == true){
+            System.out.println(event.getX() - initialPositionX);
+            //super.getSelectedShape().setTranslateX(event.getX() - initialPositionX);
+            //super.getSelectedShape().setTranslateY(event.getY() - initialPositionY);
+            getSelectedShape().relocate(event.getX() - initialPositionX,event.getY() - initialPositionY);
+        }
+        
     }
 
     @Override
-    public void onMouseReleased(MouseEvent event){
-        getExecutor().execute(new MoveCommand(getSelectedShape(),event.getX(),event.getY()));
+    public void onMouseReleased(MouseEvent event) {
+        if(flag == true){
+            this.initialPositionX = event.getX();
+            this.initialPositionY = event.getY();
+            System.out.println("Sono qui!");
+        }
     }
     
     
--- ProjectApp/src/projectapp/tools/ResizeTool.java
+++ ProjectApp/src/projectapp/tools/ResizeTool.java
@@ -4,47 +4,46 @@
  */
 package projectapp.tools;
 
-/**
- *
- * @author Sabatino
- */
-
+import javafx.event.Event;
+import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Shape;
+import projectapp.command.Command;
 import projectapp.command.CommandExecutor;
-
+import projectapp.command.MoveCommand;
 
 /**
  *
  * @author pasqualecaggiano
  */
 public class ResizeTool extends SelectionTool{
-
-    public ResizeTool(Pane pane,Shape selectedShape,CommandExecutor executor) {
-        super(pane,selectedShape,executor); 
+    
+    private double x;
+    private double y;
+    
+    public ResizeTool(Pane pane, Shape selectedShape, CommandExecutor executor) {
+        super(pane, selectedShape, executor);
+        System.out.println("sto facendo riferimento alla shape: " + selectedShape);
     }
 
     @Override
     public void onMousePressed(MouseEvent event, Color strokeColor, Color fillColor){
-        Shape selectedShape = getSelectedShape();
-        if (selectedShape != null){
-           selectedShape.setStyle("");
+        if (super.getSelectedShape() != null){
+           super.getSelectedShape().setStyle("");
         }
         
         if (event.getTarget().getClass()!= getPane().getClass()){
-            selectedShape = (Shape) event.getTarget();
-            selectedShape.setStyle("-fx-stroke-dash-array:5px");
+            super.setSelectedShape((Shape) event.getTarget());
+            super.getSelectedShape().setStyle("-fx-stroke-dash-array:5px");
         }
     }
     
     @Override
     public void onMouseDragged(MouseEvent event) {
-       
-       System.out.println(getSelectedShape().getLayoutX() + " ---" + getSelectedShape().getLayoutY());
-       getSelectedShape().setScaleX(event.getX()/getSelectedShape().getLayoutX());
-       getSelectedShape().setScaleY(event.getY()/getSelectedShape().getLayoutY());
+        super.getSelectedShape().setScaleX(event.getSceneX() / super.getSelectedShape().getLayoutBounds().getMaxX());
+        super.getSelectedShape().setScaleY(event.getSceneY() / super.getSelectedShape().getLayoutBounds().getMaxY());
     }
 
     @Override
