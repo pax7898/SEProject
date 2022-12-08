5040f79bcb3df3ed4d3a0ae95d78c4335291724d
pax7898 <pasqualecaggiano007@gmail.com>
2022-12-08 10:42:19 +0100

    Update Change color tests

--- ProjectApp/src/projectapp/command/ToFrontCommand.java
+++ ProjectApp/src/projectapp/command/ToFrontCommand.java
@@ -49,7 +49,6 @@ public class ToFrontCommand implements Command{
         pane.getChildren().remove(shape);
         pane.getChildren().add(oldIndex, shape);
         
-        //shape.toBack();
     }
     
 }
--- ProjectApp/test/projectapp/command/ChangeBorderColorCommandTest.java
+++ ProjectApp/test/projectapp/command/ChangeBorderColorCommandTest.java
@@ -35,25 +35,90 @@ public class ChangeBorderColorCommandTest {
      * Test of execute method, of class ChangeBorderColorCommand.
      */
     @Test
-    public void testExecute() {
+    public void testExecuteLine() {
         System.out.println("execute");
-        Rectangle rectangle=new Rectangle(200,200,300,300);
-        rectangle.setStroke(Color.RED);
-        command = new ChangeBorderColorCommand(rectangle,Color.BLUE);
-        command.execute();
-        assertEquals(rectangle.getStroke(),Color.BLUE);
+        
         Line line=new Line(200,200,300,300);
         line.setStroke(Color.RED);
         command = new ChangeBorderColorCommand(line,Color.BLUE);
         command.execute();
         assertEquals(line.getStroke(),Color.BLUE);
+         
+    }
+    
+    /**
+     * Test of execute method, of class ChangeBorderColorCommand.
+     */
+    @Test
+    public void testExecuteRectangle() {
+        System.out.println("execute");
+        Rectangle rectangle=new Rectangle(200,200,300,300);
+        rectangle.setStroke(Color.RED);
+        command = new ChangeBorderColorCommand(rectangle,Color.BLUE);
+        command.execute();
+        assertEquals(rectangle.getStroke(),Color.BLUE); 
+    }
+    
+    /**
+     * Test of execute method, of class ChangeBorderColorCommand.
+     */
+    @Test
+    public void testExecuteEllipse() {
+        System.out.println("execute");
+        
         Ellipse ellipse=new Ellipse(200,200,300,300);
         ellipse.setStroke(Color.RED);
         command = new ChangeBorderColorCommand(ellipse,Color.BLUE);
         command.execute();
-        assertEquals(ellipse.getStroke(),Color.BLUE);
-        
+        assertEquals(ellipse.getStroke(),Color.BLUE);   
     }
-
+    
+    /**
+     * Test of undo method, of class ChangeBorderColorCommand.
+     */
+    @Test
+    public void testUndoLine() {
+        System.out.println("undo");
+        
+        Rectangle rectangle=new Rectangle(200,200,300,300);
+        rectangle.setStroke(Color.RED);
+        command = new ChangeBorderColorCommand(rectangle,Color.BLUE);
+        command.execute();
+        command.undo();
+        assertEquals(rectangle.getStroke(),Color.RED);
+         
+    }
+    
+    /**
+     * Test of undo method, of class ChangeBorderColorCommand.
+     */
+    @Test
+    public void testUndoRectangle() {
+        System.out.println("undo");
+        
+        Line line=new Line(200,200,300,300);
+        line.setStroke(Color.RED);
+        command = new ChangeBorderColorCommand(line,Color.BLUE);
+        command.execute();
+        command.undo();
+        assertEquals(line.getStroke(),Color.RED);
+         
+    }
+    
+    /**
+     * Test of undo method, of class ChangeBorderColorCommand.
+     */
+    @Test
+    public void testUndoEllipse() {
+        System.out.println("undo");
+        
+        Ellipse ellipse=new Ellipse(200,200,300,300);
+        ellipse.setStroke(Color.RED);
+        command = new ChangeBorderColorCommand(ellipse,Color.BLUE);
+        command.execute();
+        command.undo();
+        assertEquals(ellipse.getStroke(),Color.RED);
+         
+    }
     
 }
