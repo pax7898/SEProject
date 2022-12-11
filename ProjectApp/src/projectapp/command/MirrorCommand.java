49d1161fede359975eef52ae0eaa44e2c0a71146
AlessioCoone <108277086+AlessioCoone@users.noreply.github.com>
2022-12-11 17:14:02 +0100

    update mirror

--- ProjectApp/src/projectapp/command/MirrorCommand.java
+++ ProjectApp/src/projectapp/command/MirrorCommand.java
@@ -14,20 +14,28 @@ public class MirrorCommand implements Command {
     
     private Shape shape;
     private double oldScaleX;
+    private double oldRotate;
 
     public MirrorCommand(Shape shape) {
         this.shape = shape;
+        this.oldRotate = 1;
     }
     
     @Override
     public void execute() {
         oldScaleX = shape.getScaleX();
+        System.out.println("ci sono");
         shape.setScaleX(-oldScaleX);
+        if(shape.getRotate() != 0){
+            shape.setRotate(-shape.getRotate());
+            oldRotate = -1;
+        }
     }
 
     @Override
     public void undo() {
         shape.setScaleX(oldScaleX);
+        shape.setRotate(shape.getRotate()*oldRotate);
     }
     
 }
